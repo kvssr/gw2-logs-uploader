75b90a1d4e5deba5559653c26207a1c30826ae53
kvssr <kevin.visser.info@gmail.com>
2024-06-17 16:40:01 +0200

    Changed style

--- src/components/DropzoneComponent.jsx
+++ src/components/DropzoneComponent.jsx
@@ -16,10 +16,7 @@ const baseStyle = {
   padding: "20px",
   borderWidth: 2,
   borderRadius: 2,
-  borderColor: "#eeeeee",
   borderStyle: "dashed",
-  backgroundColor: "#9daaf2",
-  color: "#ff6a3d",
   transition: "border .3s ease-in-out",
   marginBottom: 20,
 };
@@ -41,21 +38,28 @@ function DropzoneComponent(props) {
   const [fileLinks, setFilelinks] = useState([]);
   const [fileLocation, setFileLocation] = useState();
   const [parsedFile, setParsedFile] = useState();
-  const [isLoadingLinks, setIsLoadingLinks] = useState();
   const [isLoadingFile, setIsLoadingFile] = useState(false);
+  const [databaseId, setDatabaseId] = useState("");
   let fetchInterval = useRef();
 
+  console.log("🚀 ~ DropzoneComponent ~ files:", files);
   console.log("Parsed file: " + parsedFile);
   console.log("File location: " + fileLocation);
 
   const onDrop = useCallback((acceptedFiles) => {
     setFilelinks([]);
-    setFiles(
-      acceptedFiles.map((file) => {
-        uploadFile(file);
-        return file;
-      })
-    );
+    setDatabaseId("");
+    let filesTemp = {};
+    acceptedFiles.map((file) => {
+      let name = file.name.split(".")[0];
+      filesTemp[name] = {
+        file: file,
+        status: "waiting",
+        link: "",
+      };
+      setFiles({ ...filesTemp });
+      return file;
+    });
   }, []);
 
   useEffect(() => {
@@ -98,17 +102,37 @@ function DropzoneComponent(props) {
   };
 
   useEffect(() => {
-    if (files.length === fileLinks.length) setIsLoadingLinks(false);
-    else setIsLoadingLinks(true);
-  }, [files, fileLinks]);
+    for (let key in files) {
+      if (files[key]["status"] === "waiting") {
+        uploadFile(files[key]["file"], key);
+        files[key]["status"] = "loading";
+        setFiles({ ...files });
+      } else if (files[key]["status"] === "failed") {
+        uploadFile(files[key]["file"], key, "b.");
+        files[key]["status"] = "loading";
+        setFiles({ ...files });
+      }
+    }
+  }, [files]);
 
-  const uploadFile = (file) => {
+  const uploadFile = (file, name, url = "") => {
     let formData = new FormData();
     formData.append("file", file);
+    console.log("uploadFile file: ", files[name]);
 
-    fetch("https://dps.report/uploadContent?json=1&detailedwvw=true", {
+    var myHeaders = new Headers();
+    myHeaders.append("Pragma", "no-cache");
+    myHeaders.append("Cache-Control", "no-cache");
+    myHeaders.append("Connection", "keep-alive");
+    myHeaders.append("Sec-Fetch-Dest", "empty");
+    myHeaders.append("Sec-Fetch-Mode", "corse");
+    myHeaders.append("Sec-Fetch-Site", "same-site");
+
+    fetch(`https://${url}dps.report/uploadContent?json=1&detailedwvw=true`, {
       method: "POST",
       body: formData,
+      timeout: 10 * 60 * 1000,
+      headers: myHeaders,
     })
       .then((response) => console.log(response.status) || response)
       .then((response) => {
@@ -117,18 +141,71 @@ function DropzoneComponent(props) {
           console.log(response);
           return response.json();
         }
+        if (response.status > 500) {
+          throw new Error("Timeout ");
+        }
+        console.log("Diff response: " + response.status);
       })
       .then((response) => {
         // let data = response.json();
         console.log("Last then");
         console.log(response);
-        console.log(response["permalink"]);
+        console.log("Name: " + name);
+        console.log("Files: ", files);
+        files[name]["link"] = response["permalink"];
+        files[name]["status"] = "loaded";
         setFilelinks((fileLinks) => [...fileLinks, response["permalink"]]);
         console.log("files: " + files);
+      })
+      .catch((error) => {
+        console.log("Catch error", error);
+        updateLinkStatus(name, "failed");
       });
   };
 
-  const handleUploadBtnClick = async () => {
+  const updateLinkStatus = (name, status) => {
+    files[name]["status"] = status;
+    setFiles({ ...files });
+  };
+
+  const handleUploadBtnClick = () => {
+    let file = addLinksToParsedFile();
+    setDatabaseId("loading");
+    const headers = { "Content-Type": "application/json" };
+    const url = config["SERVER_URL"];
+    fetch(url + "/record/", {
+      method: "POST",
+      mode: "cors",
+      headers: headers,
+      body: JSON.stringify(file),
+    })
+      .then((response) => console.log(response.status) || response)
+      .then((response) => {
+        console.log(response);
+        if (response.status === 200) {
+          console.log("200 receiving response");
+          return response.json();
+        }
+      })
+      .then((response) => {
+        console.log("Getting response file", response);
+        setDatabaseId(response.insertedId);
+      });
+  };
+
+  const addLinksToParsedFile = () => {
+    let sortedFiles = Object.keys(files).sort();
+    console.log("🚀 ~ addLinksToParsedFile ~ sortedFiles:", sortedFiles);
+
+    let fights = parsedFile["fights"];
+    fights.forEach((fight, i) => {
+      fight["permalink"] = files[Object.keys(files)[i]]["link"];
+    });
+
+    return parsedFile;
+  };
+
+  const handleParseBtnClick = async () => {
     console.log("file links", fileLinks);
     setIsLoadingFile(true);
     const url = config["PARSER_URL"] + "/server/gwlogparser/json";
@@ -187,17 +264,59 @@ function DropzoneComponent(props) {
     [isDragActive, isDragReject, isDragAccept]
   );
 
-  const thumbs = files.map((file) => (
-    <div
-      key={file.name}
-      className="upload-thumb"
-    >
-      <img
-        src={file.preview}
-        alt={file.name}
-      />
-    </div>
-  ));
+  const Thumbs = () => {
+    let filenames = [];
+    for (let key in files) {
+      filenames.push(key);
+    }
+    return filenames.map((file) => (
+      <div
+        key={file.name}
+        className="upload-thumb"
+      >
+        {file}
+      </div>
+    ));
+  };
+
+  const ThumbsLinks = () => {
+    let filelinks = [];
+    for (let key in files) {
+      if (files[key]["status"] === "loading") filelinks.push("loading");
+      else if (files[key]["status"] === "failed") filelinks.push("failed");
+      else filelinks.push(key);
+    }
+    return filelinks.map((file) => {
+      if (file === "loading") {
+        return (
+          <div>
+            <Circles
+              width={20}
+              height={20}
+            />
+          </div>
+        );
+      } else if (file === "failed") {
+        return <div>Failed...</div>;
+      } else {
+        return (
+          <div
+            key={file + Math.floor(Math.random() * 100)}
+            className="upload-thumb"
+          >
+            {" "}
+            <a
+              href={files[file]["link"]}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {files[file]["link"]}
+            </a>
+          </div>
+        );
+      }
+    });
+  };
 
   const thumbsLinks = fileLinks.map((file) => (
     <div
@@ -215,7 +334,7 @@ function DropzoneComponent(props) {
   ));
 
   const ParsedInfo = () => {
-    if (!parsedFile) return "no file";
+    if (!parsedFile) return "";
     const stats = parsedFile["overall_raid_stats"];
     return (
       <div>
@@ -227,49 +346,96 @@ function DropzoneComponent(props) {
     );
   };
 
+  const DatabaseInfo = () => {
+    return (
+      <div>
+        <p>ID: {databaseId}</p>
+        <p>
+          Link:{" "}
+          <a
+            href={`https://kvssr.github.io/gw2log-viewer-client/#/${databaseId}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            gw2log-viewer-client
+          </a>
+        </p>
+      </div>
+    );
+  };
+
   return (
     <section>
-      <div {...getRootProps({ style })}>
+      <div
+        {...getRootProps({ style })}
+        className="dropzone"
+      >
         <input {...getInputProps()} />
         <div className="dropzone-text">Drag and drop your files here.</div>
       </div>
       <div className="resultContainer">
         <aside className="resultColumn">
           <h2>Files</h2>
-          {thumbs}
+          {/* {thumbs} */}
+          <Thumbs />
         </aside>
         <aside className="resultColumn">
           <h2>Links</h2>
-          {thumbsLinks}
-          {isLoadingLinks && <Circles />}
+          <ThumbsLinks />
         </aside>
       </div>
       <div className="parsedContainer">
-        <div className="parsedInfo">
-          {isLoadingFile && <Circles />}
-          <ParsedInfo />
-        </div>
-        <div className="parsedLink">
-          {" "}
-          {parsedFile && (
-            <a
-              href={`data:text/json;charset=utf-8,${encodeURIComponent(
-                JSON.stringify(parsedFile)
-              )}`}
-              download={`${parsedFile["overall_raid_stats"]["date"]}_wvw_log.json`}
-            >
-              {`Download Json`}
-            </a>
-          )}
+        <div className="postParsedRow">
+          <div className="parsedInfo">
+            {isLoadingFile && (
+              <Circles
+                width={20}
+                height={20}
+              />
+            )}
+            <ParsedInfo />
+
+            <div className="parsedLink">
+              {" "}
+              {parsedFile && (
+                <a
+                  href={`data:text/json;charset=utf-8,${encodeURIComponent(
+                    JSON.stringify(parsedFile)
+                  )}`}
+                  download={`${parsedFile["overall_raid_stats"]["date"]}_wvw_log.json`}
+                >
+                  {`Download Json`}
+                </a>
+              )}
+            </div>
+          </div>
+          <div className="dbContainer">
+            {databaseId === "loading" && (
+              <Circles
+                width={20}
+                height={20}
+              />
+            )}
+            {databaseId !== "loading" && databaseId !== "" && <DatabaseInfo />}
+          </div>
         </div>
       </div>
+      <div className="btnContainer">
+        <button
+          className="parseBtn btn"
+          onClick={handleParseBtnClick}
+        >
+          Parse logs
+        </button>
 
-      <button
-        className="parseBtn"
-        onClick={handleUploadBtnClick}
-      >
-        Parse logs
-      </button>
+        <button
+          className="uploadBtn btn"
+          onClick={handleUploadBtnClick}
+          disabled={!parsedFile}
+        >
+          Upload Log
+        </button>
+      </div>
     </section>
   );
 }
